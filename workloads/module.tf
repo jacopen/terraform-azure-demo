f854bf88486595a3ef76b863feb739be0e8ffb35
Kazuto Kusama <jacopen@gmail.com>
2022-03-18 05:32:03 +0000

    update

--- workloads/module.tf
+++ workloads/module.tf
@@ -7,5 +7,6 @@ module "linuxservers" {
   vnet_subnet_id      = data.terraform_remote_state.base.outputs.vnet_subnet_id
   vm_size             = "Standard_B1ls"
   remote_port         = "22"
+  ssh_key       = ""
   ssh_key_values      = [var.ssh_key_value]
 }
